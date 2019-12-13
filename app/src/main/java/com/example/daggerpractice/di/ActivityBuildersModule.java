cb0cb564ac0ac3b35d8b5ba05efa796e209198f1
Adedamola Jude Ajewole <adedamola@ito.lt>
2019-12-13 12:48:49 +0200

    -Retrofit

--- app/src/main/java/com/example/daggerpractice/di/ActivityBuildersModule.java
+++ app/src/main/java/com/example/daggerpractice/di/ActivityBuildersModule.java
@@ -5,13 +5,8 @@ import com.example.daggerpractice.di.auth.AuthModule;
 import com.example.daggerpractice.di.auth.AuthViewModelsModule;
 import com.example.daggerpractice.ui.auth.AuthActivity;
 
-import dagger.Binds;
 import dagger.Module;
-import dagger.Subcomponent;
-import dagger.android.AndroidInjector;
 import dagger.android.ContributesAndroidInjector;
-import dagger.multibindings.ClassKey;
-import dagger.multibindings.IntoMap;
 
 @Module
 public abstract class ActivityBuildersModule {
